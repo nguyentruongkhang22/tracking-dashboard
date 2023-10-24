d687495fe629b16b6e03506f3591bd4c15ce2da0
Khang <nguyentruongkhang22@gmail.com>
2023-10-24 14:24:32 +0700

    remove console log for production build

--- src/services/ws.service.js
+++ src/services/ws.service.js
@@ -2,9 +2,6 @@ export class WsService {
   static init() {
     const ws = new WebSocket("ws://localhost:3004");
     ws.onopen = function (event) {
-      console.log(" -- event: ", event);
-      console.log("Connected to WS Server");
-
       ws.send("Hello Server");
     };
   }

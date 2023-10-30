8c9f246a99ce44bb886604471eb8249183932c37
Khang <nguyentruongkhang22@gmail.com>
2023-10-31 03:38:08 +0700

    update what

--- src/services/ws.service.js
+++ src/services/ws.service.js
@@ -2,7 +2,7 @@ export class WsService {
   static url =
     process.env.NODE_ENV === "development"
       ? "ws://localhost:3003/ws"
-      : "wss://api.lackadaisical.net/ws";
+      : "ws://localhost:3003/ws";
   static init() {
     const ws = new WebSocket(this.url);
     ws.onopen = function (event) {
